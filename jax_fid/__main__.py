b768c4cb0f3489764795a5acb9fb0592b3311403
young-geng <young.gengxy@gmail.com>
2022-09-26 00:10:15 -0700

    improved dataloader

--- jax_fid/__main__.py
+++ jax_fid/__main__.py
@@ -14,27 +14,34 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--path1', type=str, help='Path to image directory or .npz file containing pre-computed statistics.')
     parser.add_argument('--path2', type=str, help='Path to image directory or .npz file containing pre-computed statistics.')
-    parser.add_argument('--batch_size', type=int, default=50, help='Batch size per device for computing the Inception activations.')
-    parser.add_argument('--img_size', type=int, nargs=2, help='Resize images to this size. The format is (height, width).')
+    parser.add_argument('--batch_size', type=int, default=128, help='Batch size per device for computing the Inception activations.')
+    parser.add_argument('--img_size', type=int, default=256, help='Resize images to this size. The format is (height, width).')
     parser.add_argument('--precompute', action='store_true', help='If True, pre-compute statistics for given image directory.')
     parser.add_argument('--img_dir', type=str, help='Path to image directory for pre-computing statistics.')
-    parser.add_argument('--out_dir', type=str, help='Path where pre-computed statistics are stored.')
+    parser.add_argument('--stats_output', type=str, help='Path where pre-computed statistics are stored.')
     args = parser.parse_args()
+
+    jax_devices = jax.local_devices()
+    n_devices = len(jax_devices)
+
+    assert args.batch_size % n_devices == 0
 
     rng = jax.random.PRNGKey(0)
 
+
     model = inception.InceptionV3(pretrained=True)
     params = model.init(rng, jnp.ones((1, 256, 256, 3)))
+    params = jax.device_put_replicated(params, jax_devices)
 
-    apply_fn = jax.jit(functools.partial(model.apply, train=False))
+    @jax.pmap
+    def apply_fn(params, x):
+        return model.apply(params, x, train=False)
 
     if args.precompute:
         assert args.img_dir is not None, 'img_dir must be specified if precompute_stats is True'
-        assert args.out_dir is not None, 'out_dir must be specified if precompute_stats is True'
+        assert args.stats_output is not None, 'stats_output must be specified if precompute_stats is True'
         mu, sigma = fid.compute_statistics(args.img_dir, params, apply_fn, args.batch_size, args.img_size)
-        os.makedirs(args.out_dir, exist_ok=True)
-        np.savez(os.path.join(args.out_dir, 'stats'), mu=mu, sigma=sigma)
-        print('Saved pre-computed statistics at:', os.path.join(args.out_dir, 'stats.npz'))
+        np.savez(args.stats_output, mu=mu, sigma=sigma)
     else:
         assert args.path1 is not None, 'path1 must be specified'
         assert args.path2 is not None, 'path2 must be specified'
--- jax_fid/fid.py
+++ jax_fid/fid.py
@@ -1,40 +1,97 @@
 import jax
 import jax.numpy as jnp
+import flax
 import numpy as np
 from PIL import Image
 import os
 import scipy
 from tqdm import tqdm
 
+import torch
 
-def compute_statistics(path, params, apply_fn, batch_size=1, img_size=None):
+
+def is_image(name):
+    image_extensions = ['.jpg', 'jpeg', '.png']
+    for ext in image_extensions:
+        if name.lower().endswith(ext):
+            return True
+    return False
+
+
+class ImageFolderDataset(torch.utils.data.Dataset):
+    def __init__(self, path, img_size=256):
+        self.path = path
+        self.img_size = img_size
+        self.image_list = [x for x in os.listdir(path) if is_image(x)]
+
+    def __len__(self):
+        return len(self.image_list)
+
+    def __getitem__(self, index):
+        img = Image.open(os.path.join(self.path, self.image_list[index]))
+
+        if img.mode == "L":
+            img = img.convert("RGB")
+        # resize if not the right size
+        if img.size[:2] != (self.img_size, self.img_size):
+            img = img.resize(
+                size=(self.img_size, self.img_size),
+                resample=Image.BILINEAR,
+            )
+        img = np.array(img) / 255.0
+        return img
+
+
+def compute_statistics(path, params, apply_fn, batch_size=1, img_size=256):
     if path.endswith(".npz"):
         stats = np.load(path)
         mu, sigma = stats["mu"], stats["sigma"]
         return mu, sigma
 
-    images = []
-    for f in tqdm(os.listdir(path)):
-        img = Image.open(os.path.join(path, f))
-        # convert if only a single channel
-        if img.mode == "L":
-            img = img.convert("RGB")
-        # resize if not the right size
-        if img_size is not None and img.size[:2] != img_size:
-            img = img.resize(
-                size=(img_size[0], img_size[1]),
-                resample=Image.BILINEAR,
-            )
-        img = np.array(img) / 255.0
-        images.append(img)
+    # images = []
+    # for f in tqdm(os.listdir(path), ncols=0):
+    #     img = Image.open(os.path.join(path, f))
+    #     # convert if only a single channel
+    #     if img.mode == "L":
+    #         img = img.convert("RGB")
+    #     # resize if not the right size
+    #     if img_size is not None and img.size[:2] != (img_size, img_size):
+    #         img = img.resize(
+    #             size=(img_size, img_size),
+    #             resample=Image.BILINEAR,
+    #         )
+    #     img = np.array(img) / 255.0
+    #     images.append(img)
 
-    num_batches = int(len(images) // batch_size)
+    # num_batches = int(len(images) // batch_size)
+    # act = []
+    # for i in tqdm(range(num_batches), ncols=0):
+    #     x = images[i * batch_size : i * batch_size + batch_size]
+    #     x = np.asarray(x)
+    #     x = 2 * x - 1
+    #     pred = apply_fn(params, jax.lax.stop_gradient(x))
+    #     act.append(pred.squeeze(axis=1).squeeze(axis=1))
+    # act = jnp.concatenate(act, axis=0)
+
+
+    dataloader = torch.utils.data.DataLoader(
+        ImageFolderDataset(path=path, img_size=img_size),
+        batch_size=batch_size, num_workers=4, drop_last=True,
+        collate_fn=lambda x: np.stack(x, axis=0)
+    )
+    def dataloader_iterator():
+        n_devices = len(jax.local_devices())
+        for x in dataloader:
+            x = 2.0 * x - 1.0
+            yield x.reshape(n_devices, -1, *x.shape[1:])
+
+    jax_dataloader = flax.jax_utils.prefetch_to_device(
+        dataloader_iterator(), size=4, devices=jax.local_devices()
+    )
     act = []
-    for i in tqdm(range(num_batches)):
-        x = images[i * batch_size : i * batch_size + batch_size]
-        x = np.asarray(x)
-        x = 2 * x - 1
-        pred = apply_fn(params, jax.lax.stop_gradient(x))
+    for x in tqdm(jax_dataloader, total=len(dataloader), ncols=0):
+        pred = jax.device_get(apply_fn(params, x))
+        pred = pred.reshape(-1, *pred.shape[2:])
         act.append(pred.squeeze(axis=1).squeeze(axis=1))
     act = jnp.concatenate(act, axis=0)
 
